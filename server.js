65aa8f33fcf78b7cb3e02b968ab1f64c1d44f61c
Giovanni <giomail33@gmail.com>
2024-01-27 21:02:03 -0500

    added file switching when entering new path name into url

--- server.js
+++ server.js
@@ -1,13 +1,17 @@
-var http = require('http');
-var fs = require('fs');
+let http = require('http');
+let fs = require('fs');
+let url = require('url');
 
-const PORT=8080; 
-
-fs.readFile('./index.html', function (err, html) {
-    if (err) throw err;    
-        http.createServer(function(request, response) {  
-            response.writeHeader(200, {"Content-Type": "text/html"});  
-            response.write(html);  
-            response.end();  
-        }).listen(PORT);
-});
+http.createServer((req, res) => {
+    let q = url.parse(req.url, true);
+    let fileName = '.' + q.pathname;
+    fs.readFile(fileName, function(err, data) {
+        if (err) {
+            res.writeHead(404, {'Content-Type': 'text/html'});
+            return res.end();
+        }
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write(data);
+        return res.end();
+    })
+})
